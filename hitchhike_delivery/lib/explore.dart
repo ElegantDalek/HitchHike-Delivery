eb4fb6cd14fdc67b43fa8c4b332c69a096f15ffe
Theodore Li <theodoreqili@gmail.com>
2020-02-15 13:31:39 -0600

    Implement multiple activities

---
+++ hitchhike_delivery/lib/explore.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class ExploreWidget extends StatelessWidget {
+  final Color color;
+
+  ExploreWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: color,);
+  }
+
+}
