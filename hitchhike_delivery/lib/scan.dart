9c5111bff52f905c7e7ef4b53b6541585bbf4a62
Theodore Li <theodoreqili@gmail.com>
2020-02-15 15:06:28 -0600

    Implement qr code scanner

--- hitchhike_delivery/lib/scan.dart
+++ hitchhike_delivery/lib/scan.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/widgets.dart';
+import 'package:qr_mobile_vision/qr_camera.dart';
 
-class ScanWidget extends StatelessWidget {
+class ScanWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ScanState();
+}
+
+class _ScanState extends State<ScanWidget> {
+  String text = "hi";
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Scan widget"));
+    return Column(
+      children: [
+        SizedBox(
+          width: 300,
+          height: 300,
+          child: QrCamera(qrCodeCallback: (code) {
+            setState(() {
+              text = code;
+            });
+          }),
+        ),
+        Text(text)
+      ]
+    );
   }
+  
 }
