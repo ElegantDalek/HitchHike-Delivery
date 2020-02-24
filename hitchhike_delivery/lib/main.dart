539a0328086cce6f095e8a69f5f10584f3837f50
Theodore Li <theodoreqili@gmail.com>
2020-02-24 15:44:07 -0600

    Add sound playing

--- hitchhike_delivery/lib/main.dart
+++ hitchhike_delivery/lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:hitchhike_delivery/profile.dart';
@@ -16,18 +19,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        accentColor: Color(0xFFFA0025),
-        primaryColor: Color(0xFFCD001F)
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          accentColor: Color(0xFFFA0025),
+          primaryColor: Color(0xFFCD001F)),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -52,7 +54,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int _pageIndex = 0;
   final List<Widget> _activities = [
     ExploreWidget(Colors.amber),
@@ -60,19 +61,23 @@ class _MyHomePageState extends State<MyHomePage> {
     ProfileWidget()
   ];
 
+  static AudioPlayer advancedPlayer = new AudioPlayer();
+  static AudioCache audioCache = new AudioCache(fixedPlayer: advancedPlayer);
+
   static bool locked = false;
   static bool isConnecting = true;
   static bool isDisconnecting = false;
   static bool get isConnected => connection != null && connection.isConnected;
-  static final TextEditingController textEditingController = new TextEditingController();
+  static final TextEditingController textEditingController =
+      new TextEditingController();
 
   // Get the instance of the bluetooth
   FlutterBluetoothSerial bluetooth = FlutterBluetoothSerial.instance;
+  static String _messageBuffer = '';
 
   static BluetoothConnection connection;
 
   static void toggleCooler() {
-    print('toggle sent!');
     if (locked) {
       _sendMessage('u');
     } else {
@@ -90,32 +95,100 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> bluetoothConnectionState() async {
     BluetoothConnection.toAddress('98:D3:32:10:F7:8B').then((_connection) {
-      print('Connected to the device');
       connection = _connection;
       setState(() {
         isConnecting = false;
         isDisconnecting = false;
       });
+
+      connection.input.listen(_onDataReceived).onDone(() {
+        // Example: Detect which side closed the connection
+        // There should be `isDisconnecting` flag to show are we are (locally)
+        // in middle of disconnecting process, should be set before calling
+        // `dispose`, `finish` or `close`, which all causes to disconnect.
+        // If we except the disconnection, `onDone` should be fired as result.
+        // If we didn't except this (no flag set), it means closing by remote.
+        if (isDisconnecting) {
+          print('Disconnecting locally!');
+        }
+        else {
+          print('Disconnected remotely!');
+        }
+        if (this.mounted) {
+          setState(() {});
+        }
+      });
     });
 
-    connection.input.listen((data) => {print('got data!')});
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and disconnect
+    if (isConnected) {
+      isDisconnecting = true;
+      connection.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
+  void _onDataReceived(Uint8List data) {
+    int backspacesCounter = 0;
+    data.forEach((byte) {
+      if (byte == 8 || byte == 127) {
+        backspacesCounter++;
+      }
+    });
+    Uint8List buffer = Uint8List(data.length - backspacesCounter);
+    int bufferIndex = buffer.length;
+
+    // Apply backspace control character
+    backspacesCounter = 0;
+    for (int i = data.length - 1; i >= 0; i--) {
+      if (data[i] == 8 || data[i] == 127) {
+        backspacesCounter++;
+      } else {
+        if (backspacesCounter > 0) {
+          backspacesCounter--;
+        } else {
+          buffer[--bufferIndex] = data[i];
+        }
+      }
+    }
+
+    // Create message if there is new line character
+    String dataString = String.fromCharCodes(buffer);
+    int index = buffer.indexOf(13);
+    if (~index != 0) {
+      audioCache.play('scream.mp3');
+      // \r\n
+      setState(() {
+        _messageBuffer = dataString.substring(index);
+      });
+
+    } else {
+      _messageBuffer = (backspacesCounter > 0
+          ? _messageBuffer.substring(
+              0, _messageBuffer.length - backspacesCounter)
+          : _messageBuffer + dataString);
+    }
   }
 
   static void _sendMessage(String text) async {
     text = text.trim();
     textEditingController.clear();
 
-    if (text.length > 0)  {
+    if (text.length > 0) {
       try {
         connection.output.add(utf8.encode(text + "\r\n"));
         await connection.output.allSent;
-      }
-      catch (e) {
+      } catch (e) {
         // Ignore error, but notify state
       }
     }
   }
-
 
   void _onBottomBarTapped(int index) {
     setState(() {
